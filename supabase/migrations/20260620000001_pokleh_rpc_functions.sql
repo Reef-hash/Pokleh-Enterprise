0c07ae8ec7794f8299a48f4329bcfb07a489b08c
Reef-hash <riffsyaz@gmail.com>
2026-06-20 11:05:11 +0800

    Fix RPC functions: LANGUAGE SQL -> plpgsql to defer table resolution

--- supabase/migrations/20260620000001_pokleh_rpc_functions.sql
+++ supabase/migrations/20260620000001_pokleh_rpc_functions.sql
@@ -3,26 +3,30 @@
 
 CREATE OR REPLACE FUNCTION public.get_total_sold_for_intake(p_intake_id UUID)
 RETURNS INTEGER
-LANGUAGE SQL
+LANGUAGE plpgsql
 STABLE
 SECURITY DEFINER
 SET search_path TO 'public'
 AS $$
-  SELECT COALESCE(SUM(s.quantity), 0)
-  FROM public.sales s
-  JOIN public.stock_distribution sd ON sd.id = s.distribution_id
-  WHERE sd.intake_id = p_intake_id;
+BEGIN
+  RETURN (SELECT COALESCE(SUM(s.quantity), 0)
+    FROM public.sales s
+    JOIN public.stock_distribution sd ON sd.id = s.distribution_id
+    WHERE sd.intake_id = p_intake_id);
+END;
 $$;
 
 CREATE OR REPLACE FUNCTION public.get_total_returned_for_intake(p_intake_id UUID)
 RETURNS INTEGER
-LANGUAGE SQL
+LANGUAGE plpgsql
 STABLE
 SECURITY DEFINER
 SET search_path TO 'public'
 AS $$
-  SELECT COALESCE(SUM(sr.quantity_returned), 0)
-  FROM public.stock_return sr
-  JOIN public.stock_distribution sd ON sd.id = sr.distribution_id
-  WHERE sd.intake_id = p_intake_id;
+BEGIN
+  RETURN (SELECT COALESCE(SUM(sr.quantity_returned), 0)
+    FROM public.stock_return sr
+    JOIN public.stock_distribution sd ON sd.id = sr.distribution_id
+    WHERE sd.intake_id = p_intake_id);
+END;
 $$;
